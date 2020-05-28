ec916e3b86b97223903ee872618948c3111b5cc1
Jonathan Cox <cox.jonathan@gmail.com>
2020-05-28 16:10:06 -0400

    adds ability to create a new blog post

--- models/contents.js
+++ models/contents.js
@@ -2,17 +2,17 @@
 module.exports = (sequelize, DataTypes) => {
   const Contents = sequelize.define('Contents', {
     user_id: DataTypes.STRING,
-    body: DataTypes.STRING,
+    body: DataTypes.STRING(50000),
     img_path: DataTypes.STRING,
     lat_location: DataTypes.INTEGER,
     long_location: DataTypes.INTEGER,
     location: DataTypes.STRING,
     date: DataTypes.DATE,
-    title:DataTypes.STRING
+    title: DataTypes.STRING
   }, {});
-  Contents.associate = function(models) {
-    Contents.belongsTo(models.User)    
-    Contents.hasMany(models.Comments)    
+  Contents.associate = function (models) {
+    Contents.belongsTo(models.User)
+    Contents.hasMany(models.Comments)
   };
   return Contents;
 };
