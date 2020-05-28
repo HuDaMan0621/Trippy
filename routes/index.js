c8d7288b5f0ce97149d5a5c22778a59d51cfe80a
hudaman0621 <mrhu828@gmail.com>
2020-05-28 11:48:02 -0400

    basic template

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'WElcome!!', 
-    user: req.session.user || null,
-  });
+    res.send('hello');
+//   res.render('../views/index', {
+      
+//     title: 'WElcome!!', 
+//     user: req.session.user || null,
+//   });
 });
 
 module.exports = router;
