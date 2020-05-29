34a574dfd3b459d4b15548f43be0893cc868abcb
Jonathan Cox <cox.jonathan@gmail.com>
2020-05-28 21:42:34 -0400

    creates ability for user to update/change their password

--- routes/profile.js
+++ routes/profile.js
@@ -17,5 +17,25 @@ router.get('/', checkAuth, (req, res, next) => {
 });
 
 //todo implement changing pw 
+router.post('/newPassword', checkAuth, (req, res, next) => {
+    let cookiePassword = req.session.user.password;
+    let cookieId = req.session.user.id;
+    console.log(req.session.user);
+    console.log(cookiePassword);
+    bcrypt.compare(req.body.currentPassword, cookiePassword, (err, match) => {
+        console.log('true')
+        if (match) {
+            console.log('match')
+            bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
+                db.User.update({ password: hash }, { returning: true, where: { id: cookieId } }
+                ).then((result) => {
+                    console.log(result);
+                    res.redirect('/homepage')
+                })
+            })
+        }
+    }
+    )
+})
 
 module.exports = router;
