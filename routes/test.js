1b1a292b2669a8799c7669a69535a6fc9f70f14d
hudaman0621 <mrhu828@gmail.com>
2020-06-01 10:27:05 -0400

    stuck on oauth

--- routes/test.js
+++ routes/test.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     // res.send('hello');
-    res.render('../views/index', {
-        title: 'Welcome!!'
+    res.render('../views/test', {
+        title: 'you are here ... Welcome!!'
     });
 });
 
